8dedeb0e92f27741b17f69869e3f0fc18dba8bac
not1cyyy <firas1120@gmail.com>
2022-09-10 02:27:20 +0100

    added the readme file and modified the code

--- Wiener.py
+++ Wiener.py
@@ -1,13 +1,31 @@
 from owiener import attack 
+import os
+from time import * 
 
+sleep(1)
 e = int(input("Enter the public exponent e: "))
 n = int(input("Enter the modulus n: "))
-c = int(input("Enter the ciphertext c: "))
-
 d = attack(e, n)
 
 if d == None:
-	print("cannot retrieve the private key")
+	sleep(1)
+	print("cannot retrieve the private key, exiting...")
+	sleep(1)
+	os._exit(1)
 else:
-	m = pow(c, d, n)
-	print(bytearray.fromhex(hex(m)[2:]).decode())
+	choice = input("Do you want to decrypt a ciphertext ? y/n : ")
+match choice:
+	case "y":
+		c = int(input("Enter the ciphertext c: "))
+		m = pow(c, d, n)
+		plain = bytearray.fromhex(hex(m)[2:]).decode()
+		sleep(1)
+		print(f'Here is the plain text: {plain}')
+		sleep(1)
+		os._exit(0)
+
+	case "n":
+		sleep(1)
+		print(f'Here is the private key: {d}, exiting....')
+		os._exit(0)
+
